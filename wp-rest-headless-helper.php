2788af087e44f600f6867b8f54fc3a92fe61711e
Michele Capichioni <capimichi@gmail.com>
2024-03-23 21:23:59 +0100

    "Updated WP Rest Headless Helper plugin information and added prevent direct file access."

---
+++ wp-rest-headless-helper.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WP Rest Headless Helper
+ * Plugin URI: https://www.michelecapicchioni.com/
+ * Description: This is a helper plugin for WP Rest API in a headless WordPress setup.
+ * Version: 1.0.0
+ * Author: Michele Capicchioni
+ * Author URI: https://www.michelecapicchioni.com/
+ * License: GPL2
+ */
+
+// Prevent direct file access
+defined('ABSPATH') or die('No script kiddies please!');
+
+// Your code goes here
+
