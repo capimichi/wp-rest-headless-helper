141ac2647feb6bc92f83a32be14b01317935cc78
Michele Capichioni <capimichi@gmail.com>
2024-03-24 09:44:36 +0100

    require_once __DIR__ . '/vendor/autoload.php';
    $moduleClasses = glob(__DIR__ . '/src/Module/*Module.php');
    
    foreach ($moduleInstances as $moduleClass) {
     $moduleClass = str_replace('.php', '', $moduleClass);
     $moduleClass = str_replace(__DIR__ . '/src/Module/', '', $moduleClass);
     $moduleClass = 'Capimichi\\WpRestHeadlessHelper\\Module\\' . $moduleClass;
     $instance = new $moduleClass();
     $moduleInstances[] = $instance;
    }
    
    usort($moduleInstances, function ($a, $b) {
     return $a->getSort() > $b->getSort() ? 1 : -1;
    });
    
    foreach ($moduleInstances as $moduleInstance) {
     $moduleInstance->initModule();
    }

--- wp-rest-headless-helper.php
+++ wp-rest-headless-helper.php
@@ -12,5 +12,27 @@
 // Prevent direct file access
 defined('ABSPATH') or die('No script kiddies please!');
 
-// Your code goes here
+require_once __DIR__ . '/vendor/autoload.php';
 
+// list classes in src/Module ending in 'Module.php'
+$moduleClasses = glob(__DIR__ . '/src/Module/*Module.php');
+
+$moduleInstances = [];
+// create an instance of each class
+foreach ($moduleClasses as $moduleClass) {
+    $moduleClass = str_replace('.php', '', $moduleClass);
+    $moduleClass = str_replace(__DIR__ . '/src/Module/', '', $moduleClass);
+    $moduleClass = 'Capimichi\\WpRestHeadlessHelper\\Module\\' . $moduleClass;
+    $instance = new $moduleClass();
+    $moduleInstances[] = $instance;
+}
+
+// sort module instances by sort order
+usort($moduleInstances, function ($a, $b) {
+    return $a->getSort() > $b->getSort() ? 1 : -1;
+});
+
+// initialize each module
+foreach ($moduleInstances as $moduleInstance) {
+    $moduleInstance->initModule();
+}
